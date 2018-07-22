e7f887e59b86e5b946ee8db56dd3a78b1d1622b6
vikram <vikrampaygude@gmail.com>
2018-07-22 23:27:21 +0530

    ui changes

--- UI/DairyProductDistributor/src/app/app-routing.module.ts
+++ UI/DairyProductDistributor/src/app/app-routing.module.ts
@@ -59,7 +59,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [ RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, {useHash: true})
+  ],
   exports: [
     RouterModule
   ],
